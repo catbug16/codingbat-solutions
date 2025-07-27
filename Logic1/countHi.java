df5931d16d9944f86fd7be3288c16fe921119d79
Catbug <duaahinai@gmail.com>
2025-07-27 12:36:30 +0400

    Added solution for countHi

---
+++ Logic1/countHi.java
@@ -0,0 +1,12 @@
+public int countHi(String str) {
+  int count =0;
+  if (str.length()>1){
+    for (int i = 0; i<str.length()-1; i++){
+    if (str.substring(i,i+2).equals("hi")){
+      count+=1;
+    }
+  }
+  }
+  return count;
+}
+
