c4e4968b3c5f79c34e981eaf847c1d1c34a2d57d
Catbug <duaahinai@gmail.com>
2025-07-28 15:36:03 +0400

    Added solution for  Warmup1-startOz

---
+++ Warmup1/startOz.java
@@ -0,0 +1,18 @@
+public String startOz(String str) {
+  if (str.length()>1){
+    if ( str.substring(0,2).equals("oz")){
+    return "oz";
+  }
+  else if (str.substring(0,1).equals("o")){
+    return "o";
+  }
+  else if (str.substring(1,2).equals("z")){
+    return "z";
+  }
+  else{
+    return "";
+  }
+  }
+  return str;
+}
+
