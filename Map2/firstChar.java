d84e9dd20fb5581362610b6f9e91aedf714940d6
Catbug <duaahinai@gmail.com>
2025-07-28 12:42:00 +0400

    Added solution for Map2-firstChar

---
+++ Map2/firstChar.java
@@ -0,0 +1,13 @@
+public Map<String, String> firstChar(String[] strings) {
+  Map<String, String> map = new HashMap<String,String>();
+  for (int  i=0; i<strings.length; i++){
+    if (map.get(strings[i].substring(0,1))!=null){
+      map.put(strings[i].substring(0,1), 
+map.get(strings[i].substring(0,1))+ strings[i]);
+    }
+    else {
+      map.put(strings[i].substring(0,1),strings[i]);
+    }
+  }
+  return map;
+}
