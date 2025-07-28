eff72099f6b0d894c296a3eb1b6dbc13785de864
Catbug <duaahinai@gmail.com>
2025-07-28 13:38:38 +0400

    Added solution for  Warmup1-posNeg

---
+++ Warmup1/posNeg.java
@@ -0,0 +1,13 @@
+public boolean posNeg(int a, int b, boolean negative) {
+  if ( a<0 && b>0 && !negative){
+    return true;
+  }
+  else if ( b<0 && a>0&& !negative){
+    return true;
+  }
+  else if (negative && a<0 && b<0 ){
+    return true;
+  }
+  return false;
+}
+
