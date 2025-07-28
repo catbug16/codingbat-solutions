2f879b786dd32bb198f6dfc240f487f4544e9197
Catbug <duaahinai@gmail.com>
2025-07-28 11:48:28 +0400

    Added solution for Map1-mapAB2

---
+++ Map1/mapAB2.java
@@ -0,0 +1,9 @@
+public Map<String, String> mapAB2(Map<String, String> map) {
+  if (map.get("a")!=null && map.get("b")!=null && 
+map.get("a").equals(map.get("b"))){
+    map.remove("a");
+    map.remove("b");
+  }
+  return map;
+}
+
