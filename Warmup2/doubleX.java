e44575f5e46bbf183384689a4bcc157c1d36dd0d
Catbug <duaahinai@gmail.com>
2025-08-01 15:34:19 +0400

    Added solution for  Warmup2-doubleX

---
+++ Warmup2/doubleX.java
@@ -0,0 +1,9 @@
+public boolean doubleX(String str) {
+  for (int i = 0; i < str.length() - 1; i++) {
+    if (str.charAt(i) == 'x') {
+      return str.charAt(i + 1) == 'x';
+    }
+  }
+  return false;
+}
+
