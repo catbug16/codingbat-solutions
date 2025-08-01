8c1209ec5c1353c243b74bb3cb5af093c13679e5
Catbug <duaahinai@gmail.com>
2025-08-01 14:19:28 +0400

    Added solution for Array-2withoutTen

---
+++ Array2/withoutTen.java
@@ -0,0 +1,12 @@
+public int[] withoutTen(int[] nums) {
+   int[] result = new int[nums.length];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 10) {
+                result[index] = nums[i];
+                index++; 
+            }
+        }
+        return result;
+}
+
