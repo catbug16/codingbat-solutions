d5b9104cfe346429d49714cd5007fc5d86c2ea8d
Catbug <duaahinai@gmail.com>
2025-08-08 17:38:07 +0400

    added solution for  String-2-groupSum6

---
+++ Recursion2/groupSum6.java
@@ -0,0 +1,13 @@
+public boolean groupSum6(int start, int[] nums, int target) {
+  if (start == nums.length) return target == 0;
+
+  if (groupSum6(start + 1, nums, target - nums[start])) return true;
+  
+  if (nums[start]!=6){
+
+  if (groupSum6(start + 1, nums, target)) return true;
+  }
+
+  return false;
+}
+
